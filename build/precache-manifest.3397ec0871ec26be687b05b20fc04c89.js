c16bdec01aebc2d3d9cc40558f4419e1cb4cff09
Suvraneel <suvra.chatterjee.neel@gmail.com>
2020-08-24 21:49:05 +0530

    third

---
+++ build/precache-manifest.3397ec0871ec26be687b05b20fc04c89.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d5baf009c19b54e8e1481d7ed14b926",
+    "url": "/todo/index.html"
+  },
+  {
+    "revision": "82c10650e81354c355fd",
+    "url": "/todo/static/css/main.ef75ab39.chunk.css"
+  },
+  {
+    "revision": "326c1195e74fbcf43e34",
+    "url": "/todo/static/js/2.1ddbefbd.chunk.js"
+  },
+  {
+    "revision": "7673ff7161c06d866a35ccc71abfab71",
+    "url": "/todo/static/js/2.1ddbefbd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "82c10650e81354c355fd",
+    "url": "/todo/static/js/main.b70f7370.chunk.js"
+  },
+  {
+    "revision": "bdb94e8564ea2a8ad029",
+    "url": "/todo/static/js/runtime-main.af8a041e.js"
+  }
+]);
